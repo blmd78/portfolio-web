feafb32e054a15cd23f32fb0f6681c10756b4c8c
BenoitArkone <benoit@arkone.fr>
2024-12-03 14:09:10 +0100

    [APP] wip update design

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -22,8 +22,8 @@ const About: React.FC = () => {
       </div>
       <h1 className="text-2xl font-bold">Ce que je fais</h1>
       <Separator className="h-1 w-[100px] rounded-full bg-[#BBB5A5] mb-4" />
-      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4">
-        <Card className="bg-[#2E2E2E] border-none text-white hover:bg-[#464646] w-full h-[150px]">
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 relative">
+        <Card className="bg-[#2E2E2E] border-none text-white hover:bg-[#464646] w-full h-[150px] z-10 opacity-90">
           <CardContent className="px-6 pt-6 h-full flex flex-col justify-between">
             <div className="flex items-start">
               <AppWindow
@@ -31,7 +31,16 @@ const About: React.FC = () => {
                 className="text-[#BBB5A5] mr-2 flex-shrink-0"
               />
               <div className="flex-1">
-                <h1 className="text-2xl font-bold">Web Design</h1>
+                <div className="relative">
+                  <h1 className="text-2xl font-bold relative z-10">
+                    <span className="relative z-10">Web Design</span>
+                    <Separator
+                      className={`absolute h-[8px] w-[${
+                        "Web Design".length * 10
+                      }px] bg-[#DF058D] bottom-2`}
+                    />
+                  </h1>
+                </div>
                 <p>
                   Je crée des interfaces web modernes et fonctionnelles, en
                   assurant un design attractif et une bonne expérience
@@ -59,7 +68,7 @@ const About: React.FC = () => {
             </div>
           </CardContent>
         </Card> */}
-        <Card className="bg-[#2E2E2E] border-none text-white hover:bg-[#464646] w-full h-[150px]">
+        <Card className="bg-[#2E2E2E] border-none text-white hover:bg-[#464646] w-full h-[150px] z-10 opacity-90">
           <CardContent className="px-6 pt-6 h-full flex flex-col justify-between">
             <div className="flex items-start">
               <CodeXml
@@ -67,7 +76,18 @@ const About: React.FC = () => {
                 className="text-[#BBB5A5] mr-2 flex-shrink-0"
               />
               <div className="flex-1">
-                <h1 className="text-2xl font-bold">Web Fullstack</h1>
+                <div className="relative">
+                  <h1 className="text-2xl font-bold relative z-10">
+                    <span className="relative z-10">Web Fullstack</span>
+                    <Separator
+                      style={{
+                        width: `${"Web Fullstack".length * 10}px`,
+                      }}
+                      className="absolute h-[8px] bg-[#2C49D8] bottom-2"
+                    />
+                  </h1>
+                </div>
+                {/* <h1 className="text-2xl font-bold">Web Fullstack</h1> */}
                 <p>
                   Je développe des applications complètes, incluant frontend et
                   le backend avec des technologies modernes.
@@ -76,7 +96,7 @@ const About: React.FC = () => {
             </div>
           </CardContent>
         </Card>
-        <Card className="bg-[#2E2E2E] border-none text-white hover:bg-[#464646] w-full h-[150px]">
+        <Card className="bg-[#2E2E2E] border-none text-white hover:bg-[#464646] w-full h-[150px] z-10 opacity-90">
           <CardContent className="px-6 pt-6 h-full flex flex-col justify-between">
             <div className="flex items-start">
               <TabletSmartphone
@@ -84,7 +104,17 @@ const About: React.FC = () => {
                 className="text-[#BBB5A5] mr-2 flex-shrink-0"
               />
               <div className="flex-1">
-                <h1 className="text-2xl font-bold">Mobile App</h1>
+                <div className="relative">
+                  <h1 className="text-2xl font-bold relative z-10">
+                    <span className="relative z-10">Mobile App</span>
+                    <Separator
+                      className={`absolute h-[8px] w-[${
+                        "Mobile App".length * 10
+                      }px] bg-[#FF6B2B] bottom-2`}
+                    />
+                  </h1>
+                </div>
+                {/* <h1 className="text-2xl font-bold">Mobile App</h1> */}
                 <p>
                   Je sais adapter les technologies web pour créer des
                   applications mobiles réactives et fonctionnelles.
@@ -93,6 +123,13 @@ const About: React.FC = () => {
             </div>
           </CardContent>
         </Card>
+        <div className="hidden  absolute md:flex justify-center items-center top-28 w-full opacity-60">
+          <img
+            src="/background-about.webp"
+            alt="profile"
+            className="rounded-lg "
+          />
+        </div>
       </div>
     </div>
   );
